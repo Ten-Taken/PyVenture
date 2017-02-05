763826cc05726192208c5152c203959e04745e9a
Ten-Taken <fleeceofhades@gmail.com>
2017-02-04 20:14:47 -0600

    Completing model API portion of course

--- django_project/testProject/tictactoe/models.py
+++ django_project/testProject/tictactoe/models.py
@@ -3,6 +3,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+GAME_STATUS_CHOICES = (
+	('A', 'Active'),
+	('F', 'First Player Wins'),
+	('S', 'Second Player Wins'),
+	('D', 'Draw')
+	)
+
 class Game(models.Model): #models.Model
 	"""docstring for Game"""
 	first_player = models.ForeignKey(User, related_name="games_first_player")
@@ -10,6 +17,10 @@ class Game(models.Model): #models.Model
 	next_to_move = models.ForeignKey(User, related_name = "games_to_move")
 	start_time = models.DateTimeField(auto_now_add=True)
 	last_active = models.DateTimeField(auto_now=True)
+	status = models.CharField(max_length = 1, default = 'A', choices=GAME_STATUS_CHOICES)
+
+	def __str__(self):
+		return "{0} vs {1}".format(self.first_player, self.second_player)
 
 class Move(models.Model): #class that inherits model, represents table of moves
 	x = models.IntegerField()
