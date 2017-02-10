17c8f6f9684c1a8f598576ce0f341eaea6b792c9
Ten-Taken <fleeceofhades@gmail.com>
2017-02-09 23:22:47 -0600

    Implemented the remainder of CRUD operations into angular controller function

--- django_env/project/scrumboard/static/scrumboard/card.directive.js
+++ django_env/project/scrumboard/static/scrumboard/card.directive.js
@@ -8,11 +8,33 @@
 	function CardDirective() {
 		return {
 			templateUrl: '/static/scrumboard/card.html',
-			restrict: 'E'
+			restrict: 'E',
+			controller: ['$scope', '$http', function($scope,$http){
+				var url = '/scrumboard/cards/' + $scope.card.id + '/';
+
+				$scope.update = function(){
+					$http.put(
+						url,
+						$scope.card
+					);
+				};
+
+				$scope.delete=function(){
+					$http.delete(url).then(function(){
+						var cards = $scope.list.cards;
+						cards.splice(cards.indexOf($scope.card), 1
+							);
+					}
+					);
+				};
+				//debounce waits for a 500 or > millisecond input pause
+				$scope.modelOptions = {debounce: 500};
+			}]
 		};
 	}
 		/*
 			E stands for Element
+			A would make the directive an attribute instead
 			This entire function basically maps itself onto an
 			html tag <scrumboardCard></scrumboardCard>
 
