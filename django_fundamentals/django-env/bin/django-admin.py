a0a9e99eeb195c96e04f13e234f87bb71d7beb7a
Greg Wolfe <fleeceofhades@gmail.com>
2018-01-12 22:24:34 -0600

    Adding Django environment and installation - not normal

---
+++ django_fundamentals/django-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/gregorywolfe/dev/learning/PyVenture/django_fundamentals/django-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
