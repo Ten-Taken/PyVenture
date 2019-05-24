a38ec15843dd4d46fb09587b9351d49019e0faf7
Gregory Wolfe <Fenrdark@gmail.com>
2019-05-24 13:44:53 -0500

    initializing views and templates

---
+++ wordcount_project/wordcount/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def eggs(request):
+    return HttpResponse('<h1>Eggs are great for you!</h1>')
